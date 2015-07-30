61c7dc760c6e73d30d255b639a916b1ed7d21e80
wpmp <wulf@it-iv.de>
2015-07-30 17:48:53 +0200

    Add support for error productmessages

---
+++ src/monto/service/error/Error.java
@@ -0,0 +1,41 @@
+package monto.service.error;
+
+import monto.service.region.Region;
+
+public class Error extends Region {
+
+    private int offset;
+    private int length;
+    private String category;
+    private String description;
+
+    public Error(int offset, int length, String category, String description) {
+        super(offset, length);
+        this.offset = offset;
+        this.length = length;
+        this.category = category;
+        this.description = description;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    @Override
+    public int getLength() {
+        return length;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", description);
+    }
+}
---
+++ src/monto/service/error/Errors.java
@@ -0,0 +1,59 @@
+package monto.service.error;
+
+import monto.service.completion.Completion;
+import monto.service.message.ParseException;
+import monto.service.message.ProductMessage;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Errors {
+
+    public static List<Error> decode(ProductMessage message) throws ParseException {
+        return decode(message.getContents().getReader());
+    }
+
+    public static List<Error> decode(Reader reader) throws ParseException {
+        try {
+            JSONArray array = (JSONArray) JSONValue.parse(reader);
+            List<Error> errors = new ArrayList<>();
+            for (Object obj : array) {
+                JSONObject encoding = (JSONObject) obj;
+                errors.add(new Error(
+                        (int) encoding.get("offset"),
+                        (int) encoding.get("length"),
+                        (String) encoding.get("category"),
+                        (String) encoding.get("description")));
+            }
+            return errors;
+        } catch (Exception e) {
+            throw new ParseException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static JSONArray encode(Stream<Error> errors) {
+        Iterator<Error> iter = errors.iterator();
+        JSONArray array = new JSONArray();
+        while (iter.hasNext()) {
+            array.add(encode(iter.next()));
+        }
+        return array;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static JSONObject encode(Error error) {
+        JSONObject object = new JSONObject();
+        object.put("offset", error.getOffset());
+        object.put("length", error.getLength());
+        object.put("category", error.getCategory());
+        object.put("description", error.getDescription());
+        return object;
+    }
+}
