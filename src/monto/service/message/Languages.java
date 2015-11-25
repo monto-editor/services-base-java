99819ef4a84e8a0cebbda83afb3ed16642be46ae
Sven Keidel <svenkeidel@googlemail.com>
2015-11-25 10:45:20 +0100

    Add classes that contain string constants for languages and products
    This should allow at least all java based services to be DRY regarding
    the string constants for languages and products.

---
+++ src/monto/service/message/Languages.java
@@ -0,0 +1,7 @@
+package monto.service.message;
+
+public class Languages {
+	public static final Language JAVA = new Language("java");
+	public static final Language JAVASCRIPT = new Language("javascript");
+	public static final Language HASKELL = new Language("haskell");
+}
---
+++ src/monto/service/message/Products.java
@@ -0,0 +1,9 @@
+package monto.service.message;
+
+public class Products {
+	public static final Product OUTLINE = new Product("outline");
+	public static final Product TOKENS = new Product("tokens");
+	public static final Product AST = new Product("ast");
+	public static final Product COMPLETIONS = new Product("completions");
+	public static final Product ERRORS = new Product("errors");
+}
