38f4fdbb6e65a4678b7d9b9bcd2f7bd1f8fede1c
Stefan <stefan@kockmann.net>
2016-01-28 13:03:23 +0100

    Added Python

--- src/monto/service/types/Languages.java
+++ src/monto/service/types/Languages.java
@@ -4,4 +4,5 @@ public class Languages {
 	public static final Language JAVA = new Language("java");
 	public static final Language JAVASCRIPT = new Language("javascript");
 	public static final Language HASKELL = new Language("haskell");
+	public static final Language PYTHON = new Language("python");
 }
