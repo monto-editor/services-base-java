c778ca7d4478eef9a060af09cc892e97c1997843
Sven Keidel <svenkeidel@googlemail.com>
2016-02-11 15:02:40 +0100

    fix whitespace in options file

--- src/monto/service/configuration/Options.java
+++ src/monto/service/configuration/Options.java
@@ -10,49 +10,49 @@ import monto.service.types.ParseException;
 
 @SuppressWarnings({ "unchecked", "rawtypes" })
 public class Options {
-	
+
     public static <T> JSONObject encode(Option<T> option) {
         JSONObject jsonObject = new JSONObject();
         option.<Void>match(simpleOption -> {
-	        	jsonObject.put("option_id", simpleOption.getOptionId());
-	        	jsonObject.put("label", simpleOption.getLabel());
-	        	jsonObject.put("type", simpleOption.getType());
-	        	jsonObject.put("default_value", simpleOption.getDefaultValue());
-	        	return null;
-	        },
-        	ignore -> { return null; }
-        	);
+                jsonObject.put("option_id", simpleOption.getOptionId());
+                jsonObject.put("label", simpleOption.getLabel());
+                jsonObject.put("type", simpleOption.getType());
+                jsonObject.put("default_value", simpleOption.getDefaultValue());
+                return null;
+            },
+            ignore -> { return null; }
+            );
         option.<Void>match(
-        		booleanOption -> { return null; },
-        		numberOption -> {
-        	        jsonObject.put("from", numberOption.getFrom());
-        	        jsonObject.put("to", numberOption.getTo());
-        			return null;
-        		},
-        		textOption -> {
-        	        jsonObject.put("regular_expression", textOption.getRegularExpression());
-        			return null;
-        		},
-        		xorOption -> {
-        	        JSONArray values = new JSONArray();
+       		booleanOption -> { return null; },
+       		numberOption -> {
+                    jsonObject.put("from", numberOption.getFrom());
+                    jsonObject.put("to", numberOption.getTo());
+                    return null;
+                },
+                textOption -> {
+                    jsonObject.put("regular_expression", textOption.getRegularExpression());
+                    return null;
+                },
+                xorOption -> {
+                    JSONArray values = new JSONArray();
         	        for (String value : xorOption.getValues()) {
         	            values.add(value);
         	        }
         	        jsonObject.put("values", values);
-        			return null;
-        		},
-        		optionGroup -> {
-        	        jsonObject.put("required_option", optionGroup.getRequiredOption());
-        	        JSONArray members = new JSONArray();
-        	        for (Option conf : optionGroup.getMembers()) {
-        	            members.add(encode(conf));
-        	        }
-        	        jsonObject.put("members", members);
-        			return null;
-        		});
+                        return null;
+                },
+                optionGroup -> {
+                    jsonObject.put("required_option", optionGroup.getRequiredOption());
+                    JSONArray members = new JSONArray();
+                    for (Option conf : optionGroup.getMembers()) {
+                        members.add(encode(conf));
+                    }
+                    jsonObject.put("members", members);
+                    return null;
+                });
         return jsonObject;
     }
-    
+
     public static Option decode(JSONObject encoding) throws ParseException {
     	try {
     		if(encoding.containsKey("option_id"))
@@ -63,7 +63,7 @@ public class Options {
     		throw new ParseException(encoding.toString(),e);
     	}
     }
-    
+
     private static Option decodeSimpleOption(JSONObject encoding) throws ParseException {
     	String optionId = (String) encoding.get("option_id");
     	String label = (String) encoding.get("label");
@@ -84,17 +84,15 @@ public class Options {
     		return new XorOption(optionId,label,(String) encoding.get("default_value"),values);
     	default:
     		throw new ParseException("unrecognized option");
-    	}    	
+    	}
     }
-    
+
     private static Option decodeOptionGroup(JSONObject encoding) throws ParseException {
     	String requiredOption = (String) encoding.get("required_option");
-    	
     	JSONArray membersJSON = (JSONArray) encoding.get("members");
     	List<Option> members = new ArrayList<>(membersJSON.size());
     	for(Object member : membersJSON)
-    		members.add(decode((JSONObject) member));
+ 		members.add(decode((JSONObject) member));
     	return new OptionGroup(requiredOption,members);
-    		
     }
 }
