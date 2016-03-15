31603f4deae43b1e1c7e9a59ad90fbc9b2a782fd
Sven Keidel <svenkeidel@googlemail.com>
2016-03-15 12:30:08 +0100

    Only allow one selection in source message

--- src/monto/service/source/SourceMessage.java
+++ src/monto/service/source/SourceMessage.java
@@ -1,7 +1,6 @@
 package monto.service.source;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
 import monto.service.product.ProductMessage;
@@ -17,18 +16,22 @@ public class SourceMessage implements Message {
     private final Source source;
     private final String content;
     private final Language language;
-    private final List<Selection> selections;
+    private final Optional<Selection> selection;
 
-    public SourceMessage(LongKey versionId, Source source, Language language, String content, Selection... selections) {
-        this(versionId, source, language, content, Arrays.asList(selections));
+    public SourceMessage(LongKey id, Source source, Language language, String content) {
+    	this(id,source,language,content,Optional.empty());
     }
 
-    public SourceMessage(LongKey id, Source source, Language language, String content, List<Selection> selections) {
+    public SourceMessage(LongKey id, Source source, Language language, String content, Selection selection) {
+    	this(id,source,language,content,Optional.of(selection));
+    }
+    
+    public SourceMessage(LongKey id, Source source, Language language, String content, Optional<Selection> selection) {
         this.id = id;
         this.source = source;
         this.language = language;
         this.content = content;
-        this.selections = selections;
+        this.selection = selection;
     }
 
     public LongKey getId() {
@@ -47,8 +50,8 @@ public class SourceMessage implements Message {
         return language;
     }
 
-    public List<Selection> getSelections() {
-        return selections;
+    public Optional<Selection> getSelection() {
+        return selection;
     }
 
 	@Override
--- src/monto/service/source/SourceMessages.java
+++ src/monto/service/source/SourceMessages.java
@@ -1,9 +1,6 @@
 package monto.service.source;
 
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -29,16 +26,14 @@ public class SourceMessages {
             final Source source = new Source((String) message.get("source"));
             final Language language = new Language((String) message.get("language"));
             final String contents = (String) message.get("contents");
-            final JSONArray selectionsArray = (JSONArray) message.getOrDefault("selections", new JSONArray());
-            final List<Selection> selections = new ArrayList<>(selectionsArray.size());
-            Iterator<JSONObject> iterator = selectionsArray.iterator();
-            while (iterator.hasNext()) {
-                final JSONObject selection = iterator.next();
-                final Long begin = (Long) selection.get("begin");
-                final Long end = (Long) selection.get("end");
-                selections.add(new Selection(begin.intValue(), end.intValue() - begin.intValue()));
+            if(message.containsKey("selection")) {
+            	final JSONObject selection = (JSONObject) message.getOrDefault("selection", new JSONArray());
+            	final Long offset = (Long) selection.get("offset");
+            	final Long length = (Long) selection.get("length");
+            	return new SourceMessage(id, source, language, contents, new Selection(offset.intValue(), length.intValue()));
+            } else {
+            	return new SourceMessage(id, source, language, contents);
             }
-            return new SourceMessage(id, source, language, contents, selections);
         } catch (Exception e) {
             throw new ParseException(e);
         }
@@ -51,14 +46,7 @@ public class SourceMessages {
         version.put("source", message.getSource().toString());
         version.put("language", message.getLanguage().toString());
         version.put("contents", message.getContent().toString());
-        JSONArray selections = new JSONArray();
-        for (Selection selection : message.getSelections()) {
-            JSONObject sel = new JSONObject();
-            sel.put("begin", selection.getStartOffset());
-            sel.put("end", selection.getEndOffset());
-            selections.add(sel);
-        }
-        version.put("selections", selections);
+        message.getSelection().ifPresent(sel -> version.put("selection", sel));
         return version;
     }
 
