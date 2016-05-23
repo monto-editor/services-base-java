2f1dae6c7ce4dc99988463fb2f69d791a64f29a5
Hans Becker <hansbecker91@gmail.com>
2016-05-23 17:55:26 +0200

    Removed Selection class (will be readded later and was the same as Region)

--- src/monto/service/source/SourceMessage.java
+++ src/monto/service/source/SourceMessage.java
@@ -1,9 +1,11 @@
 package monto.service.source;
 
 import monto.service.product.ProductMessage;
-import monto.service.types.*;
+import monto.service.types.Language;
+import monto.service.types.LongKey;
+import monto.service.types.Message;
+import monto.service.types.Source;
 
-import java.util.Optional;
 import java.util.function.Function;
 
 public class SourceMessage implements Message {
@@ -12,18 +14,12 @@ public class SourceMessage implements Message {
     private final Source source;
     private final String contents;
     private final Language language;
-    private final Selection selection;
 
     public SourceMessage(LongKey id, Source source, Language language, String contents) {
-        this(id, source, language, contents, null);
-    }
-
-    public SourceMessage(LongKey id, Source source, Language language, String contents, Selection selection) {
         this.id = id;
         this.source = source;
         this.language = language;
         this.contents = contents;
-        this.selection = selection;
     }
 
     public LongKey getId() {
@@ -40,10 +36,6 @@ public class SourceMessage implements Message {
 
     public Language getLanguage() {
         return language;
-    }
-
-    public Optional<Selection> getSelection() {
-        return Optional.ofNullable(selection);
     }
 
     @Override
