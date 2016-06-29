e178278ec861cfaa816ab4e5be230e5d74014dc9
Hans Becker <hansbecker91@gmail.com>
2016-06-29 12:17:26 +0200

    Added static create() method

--- src/main/java/monto/service/discovery/DiscoveryRequest.java
+++ src/main/java/monto/service/discovery/DiscoveryRequest.java
@@ -1,9 +1,14 @@
 package monto.service.discovery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DiscoveryRequest {
   private List<ServiceDiscover> discoverServices;
+
+  public static DiscoveryRequest create() {
+    return new DiscoveryRequest(new ArrayList<>());
+  }
 
   public DiscoveryRequest(List<ServiceDiscover> discoverServices) {
     this.discoverServices = discoverServices;
