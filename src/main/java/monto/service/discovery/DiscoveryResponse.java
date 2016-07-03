7ae1ec91cdd5ba322148d294c27ca15a0da44b6a
Hans Becker <hansbecker91@gmail.com>
2016-07-03 16:34:52 +0200

    Renamed services to serviceDescriptions Added comment about real broker json structure

--- src/main/java/monto/service/discovery/DiscoveryResponse.java
+++ src/main/java/monto/service/discovery/DiscoveryResponse.java
@@ -3,18 +3,22 @@ package monto.service.discovery;
 import java.util.List;
 
 public class DiscoveryResponse {
-  List<ServiceDescription> services;
+  /*
+   * In reality a DiscoveryResponse is a JsonArray of ServiceDescriptions. So there is no JsonObject
+   * with one serviceDescriptions property, but Gson seems to realize that on deserialization.
+   */
+  private List<ServiceDescription> serviceDescriptions;
 
-  public DiscoveryResponse(List<ServiceDescription> services) {
-    this.services = services;
+  public DiscoveryResponse(List<ServiceDescription> serviceDescriptions) {
+    this.serviceDescriptions = serviceDescriptions;
   }
 
-  public List<ServiceDescription> getServices() {
-    return services;
+  public List<ServiceDescription> get() {
+    return serviceDescriptions;
   }
 
   @Override
   public String toString() {
-    return services.toString();
+    return serviceDescriptions.toString();
   }
 }
