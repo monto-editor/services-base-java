2d06191f959b1ea3bfaf40ed8ca19525c973cd77
Hans Becker <hansbecker91@gmail.com>
2016-10-22 12:06:18 +0200

    Made command names simpler

--- src/main/java/monto/service/command/Commands.java
+++ src/main/java/monto/service/command/Commands.java
@@ -5,13 +5,13 @@ import monto.service.types.Command;
 public final class Commands {
   private Commands() {}
 
-  public static final Command CODE_COMPLETION_REQUEST = new Command("codeCompletionRequest");
+  public static final Command COMPLETE_CODE = new Command("completeCode");
 
-  public static final Command RUN_LAUNCH_CONFIGURATION = new Command("runLaunchConfiguration");
-  public static final Command DEBUG_LAUNCH_CONFIGURATION = new Command("debugLaunchConfiguration");
-  public static final Command TERMINATE_PROCESS = new Command("terminateProcess");
+  public static final Command RUN = new Command("run");
+  public static final Command DEBUG = new Command("debug");
+  public static final Command TERMINATE = new Command("terminate");
   public static final Command ADD_BREAKPOINT = new Command("addBreakpoint");
   public static final Command REMOVE_BREAKPOINT = new Command("removeBreakpoint");
-  public static final Command RESUME_DEBUGGING = new Command("resumeDebugging");
+  public static final Command DEBUG_RESUME = new Command("debugResume");
   public static final Command DEBUG_STEP = new Command("debugStep");
 }
