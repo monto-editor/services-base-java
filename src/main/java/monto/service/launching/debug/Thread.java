a387dc994857d4d696d72b523467e385965cac6b
Hans Becker <hansbecker91@gmail.com>
2016-10-24 16:37:20 +0200

    Thread now has an id, on which new equals() and hashCode() operate

--- src/main/java/monto/service/launching/debug/Thread.java
+++ src/main/java/monto/service/launching/debug/Thread.java
@@ -3,14 +3,21 @@ package monto.service.launching.debug;
 import java.util.List;
 
 public class Thread {
+  private final long id;
   private final String name;
   private final List<StackFrame> stackFrames;
   private final Breakpoint suspendingBreakpoint;
 
-  public Thread(String name, List<StackFrame> stackFrames, Breakpoint suspendingBreakpoint) {
+  public Thread(long id, String name, List<StackFrame> stackFrames,
+                Breakpoint suspendingBreakpoint) {
+    this.id = id;
     this.name = name;
     this.stackFrames = stackFrames;
     this.suspendingBreakpoint = suspendingBreakpoint;
+  }
+
+  public long getId() {
+    return id;
   }
 
   public String getName() {
@@ -28,6 +35,31 @@ public class Thread {
   @Override
   public String toString() {
     return String.format(
-        "Thread {stackFrames: %s, suspendingBreakpoint: %s}\n", stackFrames, suspendingBreakpoint);
+        "Thread {id: %d, name: %s, stackFrames: %s, suspendingBreakpoint: %s}\n", id, name,
+        stackFrames, suspendingBreakpoint);
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (int) (id ^ (id >>> 32));
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Thread other = (Thread) obj;
+    if (id != other.id)
+      return false;
+    return true;
+  }
+  
+  
 }
