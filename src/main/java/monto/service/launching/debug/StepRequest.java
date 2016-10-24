cc2f2fd7d43f3199e413b46d686584859055ff54
Hans Becker <hansbecker91@gmail.com>
2016-10-24 16:37:54 +0200

    Added StepRequest command content and ThreadStepped product

---
+++ src/main/java/monto/service/launching/debug/StepRequest.java
@@ -0,0 +1,23 @@
+package monto.service.launching.debug;
+
+public class StepRequest {
+  private final Thread thread;
+  private final StepRange range;
+
+  public StepRequest(Thread thread, StepRange range) {
+    this.thread= thread;
+    this.range = range;
+  }
+
+  public Thread getThread() {
+    return thread;
+  }
+
+  public StepRange getRange() {
+    return range;
+  }
+
+  public enum StepRange {
+    OVER, INTO, OUT
+  }
+}
--- src/main/java/monto/service/product/Products.java
+++ src/main/java/monto/service/product/Products.java
@@ -16,4 +16,5 @@ public class Products {
   public static final Product LOGICAL_SOURCE_NAME = new Product("logicalSourceName");
   public static final Product HIT_BREAKPOINT = new Product("hitBreakpoint");
   public static final Product THREADS_RESUMED = new Product("threadsResumed");
+  public static final Product THREAD_STEPPED = new Product("threadStepped");
 }
