4af528058bd4c50d025ae6d5af52f5fcad12c9aa
Hans Becker <hansbecker91@gmail.com>
2016-10-25 17:06:22 +0200

    StackFrame now includes a Source and lineNumber

--- src/main/java/monto/service/launching/debug/StackFrame.java
+++ src/main/java/monto/service/launching/debug/StackFrame.java
@@ -1,18 +1,26 @@
 package monto.service.launching.debug;
 
 import java.util.List;
+import java.util.Optional;
+import monto.service.types.Source;
 
 public class StackFrame {
-  private final String name;
+  private final Source source;
+  private final int lineNumber;
   private final List<Variable> variables;
 
-  public StackFrame(String name, List<Variable> variables) {
-    this.name = name;
+  public StackFrame(Source source, int lineNumber, List<Variable> variables) {
+    this.source = source;
+    this.lineNumber = lineNumber;
     this.variables = variables;
   }
 
-  public String getName() {
-    return name;
+  public Optional<Source> getSource() {
+    return Optional.ofNullable(source);
+  }
+
+  public int getLineNumber() {
+    return lineNumber;
   }
 
   public List<Variable> getVariables() {
