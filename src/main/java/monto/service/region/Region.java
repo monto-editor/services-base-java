57240887a8ef455e3a954653b67dd44d0d152360
Hans Becker <hansbecker91@gmail.com>
2016-11-02 16:57:46 +0100

    Added lineNumber/column to offset/length methods to Region

--- src/main/java/monto/service/region/Region.java
+++ src/main/java/monto/service/region/Region.java
@@ -1,5 +1,7 @@
 package monto.service.region;
 
+import java.util.Arrays;
+
 public class Region implements IRegion {
   private int offset;
   private int length;
@@ -25,6 +27,43 @@ public class Region implements IRegion {
 
   public String getText(String document) {
     return document.substring(getStartOffset(), getEndOffset());
+  }
+
+  public static Region fromLineNumber(String document, int startLine, int endLine) {
+    int[] offsets = getLineOffsets(document);
+    return fromLineNumberColumn(offsets, startLine, 0, endLine,
+        offsets[endLine] - offsets[endLine - 1] - 1);
+  }
+
+  public static Region fromLineNumberColumn(int[] offsets, int startLine, int startColumn,
+      int endLine, int endColumn) {
+    try {
+      int startOffset = offsets[startLine - 1] + startColumn - 1;
+      int endOffset;
+      if (endLine - 1 >= offsets.length) {
+        endOffset = offsets[offsets.length - 1] + endColumn - 1;
+      } else {
+        endOffset = offsets[endLine - 1] + endColumn - 1;
+      }
+      return new Region(startOffset, endOffset - startOffset + 1);
+    } catch (ArrayIndexOutOfBoundsException e) {
+      throw new RuntimeException(String.format("%d, %s", offsets.length, Arrays.toString(offsets)),
+          e);
+    }
+  }
+
+  public static int[] getLineOffsets(String document) {
+    String lines[] = document.split("\\r?\\n");
+    int[] offsets = new int[lines.length + 1];
+    int offset = 0;
+    offsets[0] = offset;
+    int i = 1;
+    for (String line : lines) {
+      offset += line.length() + 1;
+      offsets[i] = offset;
+      i++;
+    }
+    return offsets;
   }
 
   @Override
