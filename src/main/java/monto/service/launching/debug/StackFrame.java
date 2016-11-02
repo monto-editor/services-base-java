e423803cabdd083f7bb2955d8c641a827e75e2e4
Hans Becker <hansbecker91@gmail.com>
2016-11-02 16:57:12 +0100

    StackFrame now includes a optinal Region, that indicates the position in code

--- src/main/java/monto/service/launching/debug/StackFrame.java
+++ src/main/java/monto/service/launching/debug/StackFrame.java
@@ -2,25 +2,32 @@ package monto.service.launching.debug;
 
 import java.util.List;
 import java.util.Optional;
+import monto.service.region.Region;
 import monto.service.types.Source;
 
 public class StackFrame {
   private final Source source;
   private final int lineNumber;
+  private final Region region;
   private final List<Variable> variables;
 
-  public StackFrame(Source source, int lineNumber, List<Variable> variables) {
+  public StackFrame(Source source, int lineNumber, Region region, List<Variable> variables) {
     this.source = source;
     this.lineNumber = lineNumber;
+    this.region = region;
     this.variables = variables;
   }
 
-  public Optional<Source> getSource() {
-    return Optional.ofNullable(source);
+  public Source getSource() {
+    return source;
   }
 
   public int getLineNumber() {
     return lineNumber;
+  }
+
+  public Optional<Region> getRegion() {
+    return Optional.ofNullable(region);
   }
 
   public List<Variable> getVariables() {
